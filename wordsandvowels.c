258f07ac751eac85579887b71b9324569213548c
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-04-07 17:12:08 +0530

    String basics

---
+++ wordsandvowels.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    char A[]="How are you";
+    int i;
+    int word=1;
+    for(i=0;A[i]!='\0';i++){
+        if(A[i]=' ' && A[i--]!=' '){
+        word++;
+        }
+    }
+    printf("%d",word);
+}
