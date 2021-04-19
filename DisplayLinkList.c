72bd53f49f57151f6ee09b15405124dfbf6f1a00
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-04-19 18:48:17 +0530

    Display Linklist

--- DisplayLinkList.c
+++ DisplayLinkList.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+//Node
 struct Node{
     int data;
     struct Node *next;
 }*first=NULL;
 
-
+//create linklist
 void create(int A[],int n)          //n is number of elements in array
 {
     int i;
@@ -17,13 +17,13 @@ void create(int A[],int n)          //n is number of elements in array
     first->next=NULL;
     last=first;
     
-    for (i = 1; i < n; i++)
+    for (i = 1; i < n; i++)   //started from 1 since 0 node is created
     {
         t=(struct Node*)malloc(sizeof(struct Node));
         t->data=A[i];
         t->next=NULL;
-        last->next=t;  //?
-        last=t;   //?
+        last->next=t;  
+        last=t;   //last become the new node
     }
     
 }
