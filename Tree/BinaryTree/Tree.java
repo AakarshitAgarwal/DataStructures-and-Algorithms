25458806f18e629de144028b37ebb95bf4f8334b
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-08-05 19:27:20 +0530

    Refresh start

--- Tree/BinaryTree/Tree.java
+++ Tree/BinaryTree/Tree.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class Tree {
 
     static Scanner sc=null;
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         
         sc=new Scanner(System.in);
 
